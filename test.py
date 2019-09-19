8d20b75263c573f0bf58a14985b9675cdf432c10
Samuel Boley <30537308+samuelb97@users.noreply.github.com>
2019-09-19 18:29:57 -0400

    Weekly Stats Added

--- test.py
+++ test.py
@@ -1,30 +1,46 @@
 import nflgame
 import pickle
 
-players = None
+playersWeek1 = None
+playersWeek2 = None
+playersAllWeeks = None
 
-with open('objs.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
-    players = pickle.load(f)
+with open('2019Full.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+    playersAllWeeks = pickle.load(f)
 
+with open('2019Week1.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+    playersWeek1 = pickle.load(f)
 
-# for p in players.sort('rushing_att'):
+with open('2019Week2.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+    playersWeek2 = pickle.load(f)
+
+totalYards = 0
+for p in playersWeek2.receiving().sort('receiving_rec'):
+    if (p.receiving_rec > 0) & (p.team == "DET"):
+        totalYards += p.receiving_yds
+        msg = '%s %d Targets, %d YAC on %d receptions, %d yards, %d TDs'
+        print(msg % (p, p.receiving_tar, p.receiving_yac_yds, p.receiving_rec, p.receiving_yds, p.receiving_tds))
+        
+print("Total Yards: %d"  % totalYards)
+
+# for p in playersAllWeeks.sort('rushing_att'):
 #     if (p.rushing_att > 0):
 #         if (p.rushing_yds / p.rushing_att > 6) & (p.rushing_att > 3):
 #             msg = '%s %d carries for %d yards and %d TDs, YPC: %f'
 #             print(msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds, float(p.rushing_yds) / float(p.rushing_att)))
 
-for p in players.receiving().sort('receiving_yac_yds'):
-    if p.receiving_rec > 0:
-        if p.receiving_yac_yds / p.receiving_rec > 10:
-            msg = '%s %d YAC on %d receptions'
-            print(msg % (p, p.receiving_yac_yds, p.receiving_rec))
+# for p in playersAllWeeks.receiving().sort('receiving_yac_yds'):
+#     if p.receiving_rec > 0:
+#         if p.receiving_yac_yds / p.receiving_rec > 10:
+#             msg = '%s %d YAC on %d receptions'
+#             print(msg % (p, p.receiving_yac_yds, p.receiving_rec))
 
-# for p in players.receiving().sort('receiving_tar'):
+# for p in playersAllWeeks.receiving().sort('receiving_tar'):
 #     if p.receiving_tar > 10:
 #         if p.receiving_rec / p.receiving_tar > .7:
 #             msg = '%s %d targets for %d receptions, %d yards, and %d TDs'
 #             print(msg % (p, p.receiving_tar, p.receiving_rec, p.receiving_yds, p.receiving_tds))
 
-# for p in players.sort('rushing_att').limit(20):
+# for p in playersAllWeeks.sort('rushing_att').limit(20):
 #     msg = '%s %d carries for %d yards and %d TDs'
 #     print(msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds))
--- updateStats.py
+++ updateStats.py
@@ -4,5 +4,19 @@ import pickle
 games = nflgame.games(2019)
 players = nflgame.combine(games, plays = True)
 
-with open('objs.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+with open('2019Full.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
     pickle.dump(players, f)
+
+games = nflgame.games(2019, week=1)
+players = nflgame.combine(games, plays = True)
+
+with open('2019Week1.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+    pickle.dump(players, f)
+
+games = nflgame.games(2019, week=2)
+players = nflgame.combine(games, plays = True)
+
+with open('2019Week2.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+    pickle.dump(players, f)
+
+
