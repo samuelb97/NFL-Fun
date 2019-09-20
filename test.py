86681fbe59f73029ca4805a703acb9753be86385
Samuel Boley <30537308+samuelb97@users.noreply.github.com>
2019-09-20 16:18:49 -0400

    function organizing

--- test.py
+++ test.py
@@ -14,33 +14,59 @@ with open('2019Week1.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
 with open('2019Week2.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
     playersWeek2 = pickle.load(f)
 
-totalYards = 0
-for p in playersWeek2.receiving().sort('receiving_rec'):
-    if (p.receiving_rec > 0) & (p.team == "DET"):
-        totalYards += p.receiving_yds
-        msg = '%s %d Targets, %d YAC on %d receptions, %d yards, %d TDs'
-        print(msg % (p, p.receiving_tar, p.receiving_yac_yds, p.receiving_rec, p.receiving_yds, p.receiving_tds))
+
+
+def RecievingByTeam(players, team):
+    playerList = []
+    totalYards = 0
+    for p in players.receiving().sort('receiving_rec'):
+        if (p.receiving_rec > 0) & (p.team == team):
+            totalYards += p.receiving_yds
+            msg = '%s %d Targets, %d YAC on %d receptions, %d yards, %d TDs'
+            playerList.append(p)
+            print(msg % (p, p.receiving_tar, p.receiving_yac_yds, p.receiving_rec, p.receiving_yds, p.receiving_tds))
+            
+    print("Total Yards: %d"  % totalYards)
+    return playerList
+
+def minYPC_minRushAttempts(ypc, rushAttempts):
+    playerList =[]
+    for p in playersAllWeeks.sort('rushing_att'):
+        if (p.rushing_att > 0):
+            if (p.rushing_yds / p.rushing_att >= ypc) & (p.rushing_att >= rushAttempts):
+                msg = '%s %d carries for %d yards and %d TDs, YPC: %f'
+                playerList.append(p)
+                print(msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds, float(p.rushing_yds) / float(p.rushing_att)))
+
+    return playerList
+
+def YpcPerReceptionOver(x):
+    playerList = []
+    for p in playersAllWeeks.receiving().sort('receiving_yac_yds'):
+        if p.receiving_rec > 0:
+            if p.receiving_yac_yds / p.receiving_rec > x:
+                msg = '%s %d YAC on %d receptions'
+                playerList.append(p)
+                print(msg % (p, p.receiving_yac_yds, p.receiving_rec))
+
+    return playerList
+
+def TargetCatchPercentOver(percent):
+    playerList = []
+    for p in playersAllWeeks.receiving().sort('receiving_tar'):
+        if p.receiving_tar > 10:
+            if p.receiving_rec / p.receiving_tar > percent:
+                msg = '%s %d targets for %d receptions, %d yards, and %d TDs'
+                playerList.append(p)
+                print(msg % (p, p.receiving_tar, p.receiving_rec, p.receiving_yds, p.receiving_tds))
         
-print("Total Yards: %d"  % totalYards)
+    return playerList
 
-# for p in playersAllWeeks.sort('rushing_att'):
-#     if (p.rushing_att > 0):
-#         if (p.rushing_yds / p.rushing_att > 6) & (p.rushing_att > 3):
-#             msg = '%s %d carries for %d yards and %d TDs, YPC: %f'
-#             print(msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds, float(p.rushing_yds) / float(p.rushing_att)))
+def RushingAttemptLeaders():
+    playerList = []
+    for p in playersAllWeeks.sort('rushing_att').limit(20):
+        msg = '%s %d carries for %d yards and %d TDs'
+        playerList.append(p)
+        print(msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds))
 
-# for p in playersAllWeeks.receiving().sort('receiving_yac_yds'):
-#     if p.receiving_rec > 0:
-#         if p.receiving_yac_yds / p.receiving_rec > 10:
-#             msg = '%s %d YAC on %d receptions'
-#             print(msg % (p, p.receiving_yac_yds, p.receiving_rec))
-
-# for p in playersAllWeeks.receiving().sort('receiving_tar'):
-#     if p.receiving_tar > 10:
-#         if p.receiving_rec / p.receiving_tar > .7:
-#             msg = '%s %d targets for %d receptions, %d yards, and %d TDs'
-#             print(msg % (p, p.receiving_tar, p.receiving_rec, p.receiving_yds, p.receiving_tds))
-
-# for p in playersAllWeeks.sort('rushing_att').limit(20):
-#     msg = '%s %d carries for %d yards and %d TDs'
-#     print(msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds))
+    return playerList
